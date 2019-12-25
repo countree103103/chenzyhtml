92af297e791e27d0349740dd76af0506307528f4
countree103103 <717662837@qq.com>
2019-12-25 13:49:02 +0800

    first commit,stable verion v0.1

---
+++ static/js/basejs.js
@@ -0,0 +1,29 @@
+﻿// JavaScript Document
+		function loginClick() {
+			var name = document.login.username.value;
+			var pwd = document.login.password.value;
+			if (name == null || name.length == 0) {
+				alert("用户名不能为空！");
+				return;
+			}
+			if (pwd == null || pwd.length == 0) {
+				alert("密码不能为空！");
+				return;
+			}
+			document.login.submit();
+
+		}
+
+function registerClick() {
+	var r_name = document.register.username.value;
+	var r_pwd = document.register.password.value;
+	if (r_name == null || r_name.length == 0) {
+		alert("用户名不能为空！");
+		return;
+	}
+	if (r_pwd == null || r_pwd.length < 8) {
+		alert("密码位数不能小于8位！");
+		return;
+	}
+	document.register.submit();
+}
